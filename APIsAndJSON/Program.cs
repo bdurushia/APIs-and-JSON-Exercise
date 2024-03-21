cf9d4af446ae52cca24f21d827e5c2307771dfe1
bdurushia <b.durushia@gmail.com>
2024-03-21 14:20:17 -0500

    Completed Exercise 1, Kanye and Ron convo.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            int timer = 10;
+
+            do
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse)["quote"].ToString();
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+                Thread.Sleep(1500);
+                Console.WriteLine($"Kanye:\n{kanyeQuote}\n");
+                
+                Thread.Sleep(2000);
+                Console.WriteLine($"Ron:\n{ronQuote}\n");
+
+                timer++; 
+            
+            } while (timer > 0);
+
         }
     }
 }
