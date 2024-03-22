caf4b7f4ed5b07fe92556c3871302dee1de8a18e
bdurushia <b.durushia@gmail.com>
2024-03-22 14:45:51 -0500

    Added typing simulation to quotes.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -21,11 +21,31 @@ namespace APIsAndJSON
                 var ronResponse = client.GetStringAsync(ronURL).Result;
                 var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
 
+                Console.WriteLine("\n");
+
                 Thread.Sleep(1500);
-                Console.WriteLine($"Kanye:\n{kanyeQuote}\n");
-                
-                Thread.Sleep(2000);
-                Console.WriteLine($"Ron:\n{ronQuote}\n");
+                Console.WriteLine("Kanye: ");
+                for (int i = 0; i < kanyeQuote.Length; i++)
+                {
+                    Console.Write(kanyeQuote[i]);
+                    Thread.Sleep(10);
+                }
+
+                Console.WriteLine("\n");
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Ron: ");
+                for (int i = 0; i < ronQuote.Length; i++)
+                {
+                    Console.Write(ronQuote[i]);
+                    Thread.Sleep(10);
+                }
+
+
+                // Console.WriteLine($"Kanye:\n{kanyeQuote}\n");
+
+                //Thread.Sleep(2000);
+                //Console.WriteLine($"Ron:\n{ronQuote}\n");
 
                 timer++; 
             
